972ab9190c1f3fd03fb5df3a30e6e3c58f358b00
pogao <augusto.pedro@gmail.com>
2026-03-28 20:24:17 -0400

    correct flake configuration

--- flake.nix
+++ flake.nix
@@ -7,33 +7,37 @@
 
   outputs = { self, nixpkgs }:
     let
-      # Define our two specific systems
       macSystem = "aarch64-darwin";
       linuxSystem = "x86_64-linux";
 
-      # Function to create the Go package for a specific system
       mkLeaksFinder = pkgs: pkgs.buildGoModule {
         pname = "leaks-finder";
         version = "1.0.0";
         src = ./.;
-        # Using a fake hash to trigger the error that gives us the real one
         vendorHash = "sha256-NoDtIQZjxpQI8Z0FsfeH0fDD9T/aYJpmfAfbbX5S36s=";
       };
     in
     {
-      # Standard Nix Flake output format: packages.<system>.<name>
-      packages.${macSystem}.default = let pkgs = nixpkgs.legacyPackages.${macSystem}; in
+      packages.${macSystem}.default =
+        let
+          pkgs = nixpkgs.legacyPackages.${macSystem};
+          app = mkLeaksFinder pkgs;
+        in
         pkgs.dockerTools.buildLayeredImage {
           name = "leaks-finder";
-          contents = [ (mkLeaksFinder pkgs) pkgs.cacert ];
-          config.Cmd = [ "leaks-finder" ];
+          contents = [ app pkgs.cacert ];
+          config.Cmd = [ "${app}/bin/leaks-finder" ];
         };
 
-      packages.${linuxSystem}.default = let pkgs = nixpkgs.legacyPackages.${linuxSystem}; in
+      packages.${linuxSystem}.default =
+        let
+          pkgs = nixpkgs.legacyPackages.${linuxSystem};
+          app = mkLeaksFinder pkgs;
+        in
         pkgs.dockerTools.buildLayeredImage {
           name = "leaks-finder";
-          contents = [ (mkLeaksFinder pkgs) pkgs.cacert ];
-          config.Cmd = [ "leaks-finder" ];
+          contents = [ app pkgs.cacert ];
+          config.Cmd = [ "${app}/bin/leaks-finder" ];
         };
 
       devShells.${macSystem}.default = nixpkgs.legacyPackages.${macSystem}.mkShell {
