23b6a0797daecdf4b88f0fba8b1e35e828ce332b
pogao <augusto.pedro@gmail.com>
2026-03-28 23:13:38 -0400

    app build works and container also works

--- flake.nix
+++ flake.nix
@@ -10,35 +10,37 @@
       macSystem = "aarch64-darwin";
       linuxSystem = "x86_64-linux";
 
-      mkLeaksFinder = pkgs: pkgs.buildGoModule {
-        pname = "leaks-finder";
+      mkLinuxApp = pkgs: pkgs.buildGoModule {
+        pname = "immutable-artifact-build";
         version = "1.0.0";
         src = ./.;
         vendorHash = "sha256-NoDtIQZjxpQI8Z0FsfeH0fDD9T/aYJpmfAfbbX5S36s=";
-        CGO_ENABLED = 0;
+        env.CGO_ENABLED = 0;
+        env.GOOS = "linux";
+        env.GOARCH = "amd64";
       };
     in
     {
       packages.${macSystem}.default =
         let
           pkgs = nixpkgs.legacyPackages.${macSystem};
-          app = mkLeaksFinder pkgs;
+          app = mkLinuxApp pkgs;
         in
         pkgs.dockerTools.buildLayeredImage {
           name = "leaks-finder";
           contents = [ app pkgs.cacert ];
-          config.Cmd = [ "${app}/bin/leaks-finder" ];
+          config.Cmd = [ "${app}/bin/immutable-artifact-build" ];
         };
 
       packages.${linuxSystem}.default =
         let
           pkgs = nixpkgs.legacyPackages.${linuxSystem};
-          app = mkLeaksFinder pkgs;
+          app = mkLinuxApp pkgs;
         in
         pkgs.dockerTools.buildLayeredImage {
           name = "leaks-finder";
           contents = [ app pkgs.cacert ];
-          config.Cmd = [ "${app}/bin/leaks-finder" ];
+          config.Cmd = [ "${app}/bin/immutable-artifact-build" ];
         };
 
       devShells.${macSystem}.default = nixpkgs.legacyPackages.${macSystem}.mkShell {
